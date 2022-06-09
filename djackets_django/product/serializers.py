bf5288163053ecde47da46bfbee790eb1c9c11d4
john.njau <john.njoroge@student.moringaschool.com>
2022-06-09 22:07:02 +0300

    feature: product serializers

--- djackets_django/product/serializers.py
+++ djackets_django/product/serializers.py
@@ -15,3 +15,10 @@ class ProductSerializer(serializers.ModelSerializer):
             "get_image",
             "get_thumbnail",
         )
+        
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            
+        )
