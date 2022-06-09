abba663878a7630a86d439493b35950e27dcd496
john.njau <john.njoroge@student.moringaschool.com>
2022-06-09 22:08:39 +0300

    feature: product app models, views

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
 
 from .models import Product
@@ -12,4 +13,24 @@ class LatestProductsList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ProductDetails(APIView):
+    def get_object(self, category_slug, product_slug):
+        try :
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
